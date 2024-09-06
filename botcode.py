70337e51c0210414c505fe9b556e8f026b326928
chindimaga <rahu760@gmail.com>
2024-09-06 11:15:20 +0530

    commit to main directly

--- botcode.py
+++ botcode.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 import random
+import re
 
 
 class MatiksBot:
@@ -25,8 +26,15 @@ class MatiksBot:
             chrome_options.add_argument("--headless")  # Run in headless mode
         self.driver = webdriver.Chrome(service=Service(), options=chrome_options)
         self.driver.get(Config.url)
-        self.segid = 5
+        self.segid = -1
         self.started = False
+        # Define a regex pattern for a valid arithmetic expression without parentheses
+        self.pattern = re.compile(r'^[\d+\-*/\s]+$')
+        self.operators = ["+", "-", "×", "÷"]
+
+    def reset(self):
+        self.started = False
+        self.segid = 1000
 
     def game_menu(self) -> None:
         """
@@ -35,7 +43,7 @@ class MatiksBot:
         buttons = ["Play Now", "Play as guest"]
         for button in buttons:
             self.wait_and_click(button)
-
+        
     def wait_and_click(self, webtext: str) -> None:
         """
         Waits for an element containing the specified text to appear and clicks it.
@@ -68,6 +76,36 @@ class MatiksBot:
         except Exception as e:
             print(f"Error finding and clicking {webtext}: {e}")
 
+    def make_expression(self, content: str, checkid: int) ->str:
+        """
+        Processes the given content to extract and format a mathematical expression.
+
+        Args:
+            content (str): The content containing the mathematical expression.
+            checkid (int): The index to split the content and extract the expression.
+
+        Returns:
+            str: The formatted mathematical expression with operators replaced.
+        """
+        clean = content.split('\n', checkid)[-1].replace('\n', '')
+        return clean.replace('÷', '/').replace('×', '*')
+    
+    def is_valid_expression(self, expression: str) -> bool:
+        """
+        Validates if the given expression matches the predefined pattern for arithmetic expressions.
+
+        Args:
+            expression (str): The mathematical expression to validate.
+
+        Returns:
+            bool: True if the expression is valid, False otherwise.
+        """
+        # Check if the expression matches the pattern
+        if not self.pattern.match(expression):
+            return False
+        
+        return True
+
     def find_ans(self, content: str) -> str:
         """
         Extracts and evaluates the mathematical expression from the content.
@@ -78,15 +116,21 @@ class MatiksBot:
         Returns:
             str: The evaluated result of the expression.
         """
-        clean = content.split('\n', self.segid)[-1].replace('\n', '')
-        expression = clean.replace('÷', '/').replace('×', '*')
-        return str(int(eval(expression)))
+        expression = self.make_expression(content,self.segid)
+        if self.is_valid_expression(expression):
+            return str(int(eval(expression)))
+        else:
+            return str(random.randint(0,1000))
 
     def new_game(self) -> None:
         """
         Starts a new game by clicking the 'New Game' button.
         """
-        self.findtext_and_click('New Game')
+        body = self.get_body()
+        if 'New Game' in body:
+            self.wait_and_click('New Game')
+        elif 'Go Home' in body:
+            self.wait_and_click('Go Home')
 
     def get_body(self) -> str:
         """
@@ -96,28 +140,52 @@ class MatiksBot:
             str: The text content of the body element.
         """
         return self.driver.find_element(By.XPATH, "/html/body").text
+    
+    def set_segid(self, content):
+        """
+        Sets the segment ID (segid) based on the position of the first operator in the content.
+
+        Args:
+            content (str): The content to search for operators.
+
+        Returns:
+            None
+        """
+        # Iterate through the string and check for operators
+        lst = content.split('\n')
+        n = len(lst)
+        while(not lst[n-1].isdigit() and n>=0):
+            n-=1
+        for i in range(len(lst) - 1):
+            if not ((lst[i].isdigit() and lst[i-1] in self.operators) or (lst[i-1].isdigit() and lst[i] in self.operators)):
+                self.segid = i
+                return
+        return
 
     def play(self) -> None:
         """
         Automates the gameplay by solving the mathematical expressions.
         """
+        self.reset()
         self.wait_and_click('Starting')
 
         while True:
             content = self.get_body()
+            # print(content)
             if not self.started and 'Starting' in content:
                 print("detected start")
                 self.power_nap()
-            elif 'New Game' in content or 'solved' in content:
-                break
-            else:
+            elif not ('YOUR' in content and 'SCORE' in content):
                 if not self.started:
                     inputfield = self.driver.find_element(By.XPATH, "//input[contains(@placeholder,'Please enter your answer here')]")
+                    self.set_segid(content)
                 self.started = True
                 ans = self.find_ans(content)
                 print(f"ans = {ans}")
                 inputfield.send_keys(ans)
                 self.power_nap()
+            else:
+                break    
 
     def close(self) -> None:
         """
--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     Configuration class for MatiksBot.
     """
     url: str = "https://www.matiks.in/"
-    botspeed_minmax: Tuple[int, int] = (1, 3)
+    botspeed_minmax: Tuple[int, int] = (0.8, 1.5)
     headless_browser = False
     pageload_waittime = 10
